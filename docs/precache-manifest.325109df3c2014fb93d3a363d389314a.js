4a6ea8c33aafa0a8e2ae661936f6f7fa325341fc
Raúl González <ragoal@gmail.com>
2020-09-25 19:17:06 +0200

    docs agregado

---
+++ docs/precache-manifest.325109df3c2014fb93d3a363d389314a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0b14e66b61960cae24d5bb0b38ad14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "194e351c1dcf3d6260c2",
+    "url": "/static/css/main.2c3ee891.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "194e351c1dcf3d6260c2",
+    "url": "/static/js/main.9e947dc3.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
